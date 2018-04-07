3587f900837268d74324f2076d19d0f202626d6f
Taylor Edwards <Taylor.Edwards68@T-Mobile.com>
2018-04-07 11:20:33 -0700

    Serve arbitrary assets from ./ui/build

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,23 @@ use rouille::Response;
 
 fn main() {
     let index = include_str!("../ui/build/index.html");
-    let bundle = include_str!("../ui/build/static/js/main.ca62a29f.js");
-    let styles = include_str!("../ui/build/static/css/main.c17080f1.css");
-    let logo = include_str!("../ui/build/static/media/logo.5d5d9eef.svg");
-    let sw = include_str!("../ui/build/service-worker.js");
 
     rouille::start_server("0.0.0.0:5000", move |request| {
-
-        let response = router!(request,
-            (GET) ["/"] => {
-                Response::html(index)
-            },
-            (GET) ["/static/js/main.ca62a29f.js"] => {
-                Response::text(bundle)
-            },
-            (GET) ["/static/css/main.c17080f1.css"] => {
-                Response::text(styles)
-            },
-            (GET) ["/static/media/logo.5d5d9eef.svg"] => {
-                Response::text(logo)
-            },
-            (GET) ["/service-worker.js"] => {
-                Response::text(sw)
-            },
-            _ => {
-                Response::empty_404()
-            }
-        );
-
-        response
+        {
+            let response = router!(request,
+                (GET) ["/"] => {
+                    Response::html(index)
+                },
+                _ => {
+                    let asset = rouille::match_assets(&request, "./ui/build/");
+                    if asset.is_success() {
+                        return asset;
+                    }
+                    return Response::empty_404();
+                }
+            );
+ 
+            return response;
+        }
     });
 }
